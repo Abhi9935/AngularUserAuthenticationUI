90be504436a7527c0cfa7041a7bcaee5163e65f1
Abhi9935 <kushwahaabinav96@gmail.com>
2026-08-30 00:36:04 +0530

    Loginand Register UI Implementaion

--- ResumeBuilder.UI/src/app/pages/login/login.ts
+++ ResumeBuilder.UI/src/app/pages/login/login.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { LoginRequest } from '../../models/login-request';
 
 @Component({
-  imports: [],
   selector: 'app-login',
-  styleUrl: './login.css',
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
+  styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+  isSubmitting = false;
+  showPassword = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const request: LoginRequest = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+
+    console.log('Login Request:', request);
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+}
